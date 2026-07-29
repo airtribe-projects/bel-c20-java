b8f324a319bda5d4a14d0cfaa1997f6d0646643c
piyushpawan0000 <34138285+piyushpawan0000@users.noreply.github.com>
2026-07-29 19:44:30 +0530

    Add files via upload

--- LearnerManagementSystemBELC20/src/main/java/org/airtribe/LearnerManagementSystemBELC20/LearnerManagementSystemBelc20Application.java
+++ LearnerManagementSystemBELC20/src/main/java/org/airtribe/LearnerManagementSystemBELC20/LearnerManagementSystemBelc20Application.java
@@ -2,8 +2,11 @@ package org.airtribe.LearnerManagementSystemBELC20;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
 
 @SpringBootApplication
+//@EnableCaching
 public class LearnerManagementSystemBelc20Application {
 
 	public static void main(String[] args) {
